b0fb5d46dbe85404435366baa9a5bc1b0acc9b43
LLQiangwei <m17773374621@163.com>
2024-10-30 21:03:06 +0800

    学校的第六周编程作业

---
+++ 第六周作业/第六周作业/第六周作业1.c
@@ -0,0 +1,20 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int a = 0,b = 0,i = 0,gcd = 0;
+//	
+//	scanf("%d %d",&a,&b);
+//
+//	for(i = 1;i <= a||i <= b;i++)
+//	{
+//		if(a % i == 0&&b % i == 0)
+//		{
+//			gcd = i;
+//		}
+//	}
+//
+//	printf("GCD=%d\n",gcd);
+//
+//	return 0;
+//}
